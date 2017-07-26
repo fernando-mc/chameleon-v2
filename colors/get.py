15f8e4c6a86486875b7b03ab1e652808f97344d2
Fernando Medina Corey - pub <fmcorey@gmail.com>
2017-07-25 19:39:31 -0700

    Abandon Designseeds, add starter images

---
+++ colors/get.py
@@ -0,0 +1,25 @@
+import os
+import json
+
+from todos import decimalencoder
+import boto3
+dynamodb = boto3.resource('dynamodb')
+
+
+def get(event, context):
+    table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
+
+    # fetch color from the database
+    result = table.get_item(
+        Key={
+            'id': event['pathParameters']['id']
+        }
+    )
+
+    # create a response
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(result['Item'])
+    }
+
+    return response
